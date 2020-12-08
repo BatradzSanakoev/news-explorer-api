ba2e824166104d0f480f50f6d9d3737ffae6ebac
Bato <sanakoev.batraz@yandex.ru>
2020-12-08 14:28:28 +0300

    fix: правки

--- app.js
+++ app.js
@@ -34,7 +34,18 @@ const limiter = rateLimit({
 //   credentials: true
 // };
 
-app.use(cors());
+const allowlist = ['http://example1.com', 'http://example2.com']
+const corsOptionsDelegate = (req, callback) => {
+  let corsOptions;
+  if (allowlist.indexOf(req.header('Origin')) !== -1) {
+    corsOptions = { origin: true }; // reflect (enable) the requested origin in the CORS response
+  } else {
+    corsOptions = { origin: false }; // disable CORS for this request
+  }
+  callback(null, corsOptions); // callback expects two parameters: error and options
+};
+
+app.use(cors(corsOptionsDelegate));
 app.use(cookieParser());
 app.use(limiter);
 app.use(bodyParser.json());
